2f8ed5287939f747c5c59be49b89cb47d6cb883d
buffermaker <www.cctv1688@qq.com>
2019-08-04 19:40:02 +0800

    clear release 1.0 files

--- build/deploy-ci.sh
+++ build/deploy-ci.sh
@@ -12,19 +12,22 @@ if [ "$TRAVIS_TAG" ]; then
   git clone https://github.com/BufferMaker/buffermaker.github.io.git && cd buffermaker.github.io
   git tag $TRAVIS_TAG -m "[build] $TRAVIS_TAG"
   git push origin $TRAVIS_TAG
+  cd ../..
 
   # build site
   npm run build:site
+  cd temp_web/buffermaker.github.io
+  git checkout gh-pages
   # build sub folder
   echo $TRAVIS_TAG
 
   SUB_FOLDER='v1.0'
   TRAVIS_COMMIT_MSG="[deploy] $(git log --format='%h - %B' --no-merges -n 1)"
-  mkdir SUB_FOLDER
+  mkdir $SUB_FOLDER
   rm -rf *.js *.css *.map static
   rm -rf $SUB_FOLDER/**
-  cp -rf dist/** .
-  cp -rf dist/** $SUB_FOLDER/
+  cp -rf ../../dist/** .
+  cp -rf ../../dist/** $SUB_FOLDER/
   git add -A .
   git commit -m "$TRAVIS_COMMIT_MSG"
   git push origin gh-pages
